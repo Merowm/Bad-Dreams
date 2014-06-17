db49da9c55a77696655909b1f196bdb608a8db06
Lasse Malmberg <lmimalmberg@gmail.com>
2014-06-17 13:34:18 +0300

    Main Menu scene stuff Spider animation

---
+++ Bad Dreams/Assets/Game Objects/Main Menu/MainMenuState/MainMenuStateManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuStateManager : MonoBehaviour
+{
+    private static MainMenuStateSystem MainMenuStateSystem { get; set; }
+
+    static MainMenuStateManager()
+    {
+        MainMenuStateSystem = GameObject.FindObjectOfType<MainMenuStateSystem>();
+    }
+
+    public static void SwitchTo(MainMenuState mainMenuState)
+    {
+        MainMenuStateSystem.SwitchTo(mainMenuState);
+    }
+
+    public static void UpdateReferences()
+    {
+        MainMenuStateSystem = GameObject.FindObjectOfType<MainMenuStateSystem>();
+    }
+}
---
+++ Bad Dreams/Assets/Game Objects/Main Menu/MainMenuState/MainMenuStateSystem.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenuStateSystem : MonoBehaviour
+{
+    public MainMenuState CurrentState { get; private set; }
+
+    private List<GameObject> MainObjects { get; set; }
+    private List<GameObject> CreditsObjects { get; set; }
+
+    private void Start()
+    {
+        SwitchTo(MainMenuState.Main);
+        GameplayStateManager.UpdateReferences();
+
+        GetMainObjects();
+        GetCreditsObjects();
+    }
+
+    private void GetMainObjects()
+    {
+        MainObjects = new List<GameObject>();
+
+        MainObjects.Add(GameObject.Find("Main UI"));
+        MainObjects.Add(GameObject.Find("Main Objects"));
+    }
+
+    private void GetCreditsObjects()
+    {
+        CreditsObjects = new List<GameObject>();
+
+        CreditsObjects.Add(GameObject.Find("Credits UI"));
+        CreditsObjects.Add(GameObject.Find("Credits Objects"));
+        SetGameObjectsActive(CreditsObjects, false);
+    }
+
+    public void SwitchTo(MainMenuState state)
+    {
+        CurrentState = state;
+        OnSwitch(state);
+    }
+
+    private void OnSwitch(MainMenuState state)
+    {
+        switch (state)
+        {
+            case MainMenuState.Main:
+                SwitchToMain();
+                break;
+
+            case MainMenuState.LevelSelection:
+                SwitchToLevelSelection();
+                break;
+
+            case MainMenuState.Options:
+                SwitchToOptions();
+                break;
+
+            case MainMenuState.Credits:
+                SwitchToCredits();
+                break;
+        }
+    }
+
+    private void SwitchToMain()
+    {
+        SetGameObjectsActive(CreditsObjects, true);
+        SetGameObjectsActive(MainObjects, false);
+    }
+
+    private void SwitchToLevelSelection()
+    {
+    }
+
+    private void SwitchToOptions()
+    {
+    }
+
+    private void SwitchToCredits()
+    {
+        SetGameObjectsActive(MainObjects, false);
+        SetGameObjectsActive(CreditsObjects, true);
+    }
+
+    private void SetGameObjectsActive(List<GameObject> gameObjects, bool active)
+    {
+        for (int i = 0; i < gameObjects.Count; ++i)
+            gameObjects[i].SetActive(active);
+    }
+}
