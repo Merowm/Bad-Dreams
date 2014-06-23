fa3612664e8e81ae31171480cf812d106aa9328c
Lasse Malmberg <lmimalmberg@gmail.com>
2014-06-23 09:10:38 +0300

    Possible spider bug fix

--- Bad Dreams/Assets/Game Objects/Game/Enemies/Spider/SpiderCollision.cs
+++ Bad Dreams/Assets/Game Objects/Game/Enemies/Spider/SpiderCollision.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class SpiderCollision : MonoBehaviour
 {
+    private SpiderAI spider;
+
+    private void Start()
+    {
+        spider = GetComponent<SpiderAI>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.name == "Player")
-        {
             GameplayStateManager.SwitchTo(GameplayState.GameOver);
-        }
+
+        if (other.gameObject.name == "Spider Web")
+            spider.SwitchTo(SpiderAIState.Idle);
     }
 }
