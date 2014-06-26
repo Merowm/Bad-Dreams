a655ff2e6d4f49ec88c9c4012c7ffdc75f8bf01a
Ville Ronkainen <finvpower@gmail.com>
2014-06-26 10:25:19 +0300

    Changes to sound options system: Instead of saving to a global Options gameobject, sound levels are now saved straight to PlayerPrefs.

--- Bad Dreams/Assets/Game Objects/Options/Music.cs
+++ Bad Dreams/Assets/Game Objects/Options/Music.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿//
+//ADD THIS SCRIPT TO ANY BACKGROUND MUSIC
+//
+
+using UnityEngine;
 using System.Collections;
 
 public class Music : MonoBehaviour 
 {
 	void Start () 
     {
+        audio.volume = PlayerPrefs.GetFloat("music", 1.0f);
+
         audio.ignoreListenerVolume = true;
         gameObject.tag = "Music";
 	}
---
+++ Bad Dreams/Assets/Game Objects/Options/SoundSlider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSlider : MonoBehaviour {
+
+    UISlider sound;
+    const float defaultVolume = 1.0f;
+
+	void Start() 
+    {
+        sound = GetComponent<UISlider>();
+        sound.value = PlayerPrefs.GetFloat("sound", defaultVolume);
+        AudioListener.volume = sound.value;
+	}
+
+    public void SaveSoundLevel()
+    {
+        PlayerPrefs.SetFloat("sound", sound.value);
+        AudioListener.volume = sound.value;
+    }
+}
