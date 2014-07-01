f191afeafd6edfdc8e5b64e707bb7d2d14d0e124
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-01 10:10:50 +0300

    Level Finished stuff

---
+++ Bad Dreams/Assets/Game Objects/Game/UI/Scripts/Buttons/LevelFinishedContinue.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelFinishedContinue : MonoBehaviour
+{
+    private Transition transition;
+
+    private void Start()
+    {
+        transition = GameObject.Find("Transition").GetComponent<Transition>();
+    }
+
+    private void OnClick()
+    {
+        transition.PlayForward();
+        transition.GetComponent<TweenScale>().AddOnFinished(new EventDelegate(this, "SwitchToLevelSelect"));
+    }
+
+    private void SwitchToLevelSelect()
+    {
+        transition.GetComponent<TweenScale>().RemoveOnFinished(new EventDelegate(this, "SwitchToLevelSelect"));
+        Application.LoadLevel("MainMenu");
+        transition.PlayReverse();
+    }
+}
