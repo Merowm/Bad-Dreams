f259d36751b1319744480017c936eb85f9385180
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-03 12:44:19 +0300

    Drop Counter HUD

---
+++ Bad Dreams/Assets/Game Objects/Game/HUD/Drop Counter/DropCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropCounter : MonoBehaviour
+{
+    private int dropCount;
+    private UILabel uiLabel;
+
+    private void Start()
+    {
+        dropCount = 0;
+        uiLabel = GameObject.Find("Drop Counter Text").GetComponentInChildren<UILabel>();
+    }
+
+    public int DropCount
+    {
+        get { return dropCount; }
+        set
+        {
+            dropCount = Mathf.Clamp(value, 0, int.MaxValue);
+            uiLabel.text = dropCount.ToString();
+        }
+    }
+}
