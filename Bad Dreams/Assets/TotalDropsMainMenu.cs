24f03204bd8a98c653c03d8623cbc9f580ae3a2d
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-03 15:47:13 +0300

    Total drops in level select

---
+++ Bad Dreams/Assets/TotalDropsMainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using SaveSystem;
+
+public class TotalDropsMainMenu : MonoBehaviour
+{
+    private UILabel uiLabel;
+    private bool statusUpdated;
+
+    private void Start()
+    {
+        uiLabel = GetComponentInChildren<UILabel>();
+
+        if (SaveManager.CurrentSave != null)
+        {
+            uiLabel.text = SaveManager.CurrentSave.Drops.ToString();
+        }
+    }
+
+    private void Update()
+    {
+        if (!statusUpdated)
+        {
+            uiLabel.text = SaveManager.CurrentSave.Drops.ToString();
+            statusUpdated = true;
+        }
+    }
+
+    private void OnEnable()
+    {
+        statusUpdated = false;
+    }
+}
