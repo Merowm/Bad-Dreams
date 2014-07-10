9029b10075a097e9f759bda030c5eca23d8cc237
Ville Ronkainen <finvpower@gmail.com>
2014-07-10 14:23:30 +0300

    There once was a trap called dripper - He was just a jolly young skipper - He didn't mean harm - To the fairy so calm - His acid, though, quite the tripper

---
+++ Bad Dreams/Assets/Game Objects/Game/Enemies/Dripper/AcidBall.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcidBall : MonoBehaviour 
+{
+    const int LAYER_TERRAINCOLLISION = 8;
+    const int LAYERMASK = 1 << 8;
+    const float MAX_SPEED = 100.0f;
+
+    float speed;
+    bool speedCapped;
+    float scale;
+
+    bool falling;
+
+    Animator anim;
+    SpriteRenderer spriteRend;
+
+	// Use this for initialization
+	void Start () 
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(FadeToExistence(3.0f));
+        name = "Acid Ball";
+        speed = 0;
+        falling = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (falling)
+        {
+            if (!speedCapped)
+            {
+                speed += Time.deltaTime * 9.81f; //blasting off at the speed of sound
+                if (speed >= MAX_SPEED)
+                {
+                    speedCapped = true;
+                }
+            }
+
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+
+            if (Physics2D.Raycast(transform.position, -Vector2.up, transform.localScale.x * 0.7f, LAYERMASK))
+            {
+                OnHitGround();
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            if (col.gameObject.GetComponentInChildren<SpriteRenderer>().sortingLayerName == "Player Foreground")
+            {
+                OnHitPlayer();
+            }
+        }
+    }
+
+    void OnHitGround()
+    {
+        falling = false;
+        Destroy(this.collider2D);
+        AcidSplash();
+    }
+
+    void OnHitPlayer()
+    {
+        falling = false;
+        Destroy(this.gameObject);
+        GameObject.Find("Player").GetComponent<HitAnimation>().ActivateAnimation();
+    }
+
+    void AcidSplash()
+    {
+        Sound();
+        anim.SetBool("hit", true);
+    }
+
+    void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void Sound()
+    {
+        //add sound effect of acid/ice hitting the ground/player playing here
+    }
+
+    IEnumerator FadeToExistence(float timeMultiplier)
+    {
+        for (float t = 0.0f; t <= 1.2f; t += Time.deltaTime * timeMultiplier)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(0, 1, t));
+            spriteRend.color = newColor;
+            yield return null;
+        }
+    }
+}
---
+++ Bad Dreams/Assets/Game Objects/Game/Enemies/Dripper/Dripper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dripper : MonoBehaviour 
+{
+    public float acidDelay;
+    bool acidDelayOn;
+    GameObject acidBall;
+
+	// Use this for initialization
+	void Start () 
+    {
+        acidBall = Resources.Load("Enemies/Acidball") as GameObject;
+        acidDelayOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!acidDelayOn)
+        {
+            DropAcid();
+            acidDelayOn = true;
+            Invoke("AcidDelay", acidDelay);
+        }
+	}
+
+    void DropAcid()
+    {
+        GameObject ball = Instantiate(acidBall, transform.position, Quaternion.identity) as GameObject;
+        ball.transform.parent = transform;
+    }
+
+    void AcidDelay()
+    {
+        acidDelayOn = false;
+    }
+}
