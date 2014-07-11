0a78b71c82725374abdddb163bf910832520668d
Ville Ronkainen <finvpower@gmail.com>
2014-07-11 12:01:34 +0300

    Acid dripper changes. New spike trap, drops spikes from ceiling.

--- Bad Dreams/Assets/Game Objects/Game/Enemies/Dripper/Dripper.cs
+++ Bad Dreams/Assets/Game Objects/Game/Enemies/Dripper/Dripper.cs
@@ -6,10 +6,12 @@ public class Dripper : MonoBehaviour
     public float acidDelay;
     bool acidDelayOn;
     GameObject acidBall;
+    Transform tip;
 
 	// Use this for initialization
 	void Start () 
     {
+        tip = transform.Find("Tip");
         acidBall = Resources.Load("Enemies/Acidball") as GameObject;
         acidDelayOn = false;
 	}
@@ -27,7 +29,7 @@ public class Dripper : MonoBehaviour
 
     void DropAcid()
     {
-        GameObject ball = Instantiate(acidBall, transform.position, Quaternion.identity) as GameObject;
+        GameObject ball = Instantiate(acidBall, tip.position, Quaternion.identity) as GameObject;
         ball.transform.parent = transform;
     }
 
