88b89b62e367a1066886b2d6a9237a27752dbebf
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-14 14:57:19 +0300

    Selected menu button scaling

---
+++ Bad Dreams/Assets/Game Objects/Main Menu/UI/Buttons/ButtonSelectedScale.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSelectedScale : MonoBehaviour
+{
+    private TweenScale tweenScale;
+
+    private void Start()
+    {
+        tweenScale = GetComponent<TweenScale>();
+    }
+
+    public void Update()
+    {
+        if (UICamera.selectedObject != null && UICamera.hoveredObject != null)
+            UICamera.selectedObject = null;
+
+        if (UICamera.hoveredObject == this.gameObject)
+        {
+            UICamera.selectedObject = null;
+            tweenScale.enabled = true;
+        }
+        if (UICamera.selectedObject == this.gameObject)
+        {
+            UICamera.hoveredObject = null;
+            tweenScale.enabled = true;
+        }
+        
+
+        if (UICamera.hoveredObject != this.gameObject &&
+            UICamera.selectedObject != this.gameObject &&
+            tweenScale.enabled)
+        {
+            if (tweenScale.direction == AnimationOrTween.Direction.Forward)
+            {
+                tweenScale.ResetToBeginning();
+                tweenScale.enabled = false;
+            }
+        }
+
+
+        //if (UICamera.selectedObject == this.gameObject)
+        //{
+        //    UICamera.hoveredObject = null;
+        //    tweenScale.enabled = true;
+        //}
+        //if (UICamera.hoveredObject == this.gameObject)
+        //{
+        //    UICamera.selectedObject = null;
+        //    tweenScale.enabled = true;
+        //}
+
+        //if (UICamera.hoveredObject != this.gameObject && 
+        //    UICamera.selectedObject != this.gameObject &&
+        //    tweenScale.enabled)
+        //{
+        //    if (tweenScale.direction == AnimationOrTween.Direction.Forward)
+        //    {
+        //        tweenScale.ResetToBeginning();
+        //        tweenScale.enabled = false;
+        //    }
+        //}
+    }
+}
