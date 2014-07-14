781ce153aacd69dc560554cbbac4e363095d4231
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-14 13:22:06 +0300

    Smooth menus

---
+++ Bad Dreams/Assets/Game Objects/Main Menu/UI/Buttons/ButtonFade.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonFade : MonoBehaviour
+{
+    private UISprite uiSprite;
+    private bool fadeIn;
+    private float fadeSpeed;
+
+    private void Start()
+    {
+        uiSprite = GetComponent<UISprite>();
+        fadeSpeed = 0.1F;
+    }
+
+    private void Update()
+    {
+        if (fadeIn)
+        {
+            if (uiSprite.color.a < 1)
+            {
+                uiSprite.color = new Color(
+                    uiSprite.color.r,
+                    uiSprite.color.g,
+                    uiSprite.color.b,
+                    uiSprite.color.a + fadeSpeed);
+            }
+            else
+            {
+                fadeIn = false;
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        fadeIn = true;
+    }
+
+    private void OnDisable()
+    {
+        if (uiSprite != null)
+        {
+            uiSprite.color = new Color(
+                uiSprite.color.r,
+                uiSprite.color.g,
+                uiSprite.color.b,
+                0);
+        }
+    }
+}
