acfe3d62f6025b6a5abf7a28047c68ea9617becd
Pauli Vuollet <liquidsilk2@gmail.com>
2014-07-16 15:38:24 +0300

    Cleaned up some hierarchy in snow level scene Lethal spikes are 99% lethal

---
+++ Bad Dreams/Assets/Game Objects/Game/Enemies/Lethal Spikes/LethalSpikeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalSpikeTrigger : MonoBehaviour
+{
+	public string triggerType;
+	public GameObject myParent;
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.name == "Player")
+		{
+			if (myParent)
+			{
+				myParent.SendMessage("ReceiveTrigger", triggerType);
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c)
+	{
+		if (c.gameObject.name == "Player")
+		{
+			if (myParent)
+			{
+				myParent.SendMessage("ReceiveTriggerExit", triggerType);
+			}
+		}
+	}
+}
