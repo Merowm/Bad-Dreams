07366304580161d284fe26cfc42ea6d3ff911df2
Lasse Malmberg <lmimalmberg@gmail.com>
2014-07-23 17:18:39 +0300

    Asset page to credits

---
+++ Bad Dreams/Assets/Game Objects/Main Menu/UI/Buttons/CreditsChangePage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsChangePage : MonoBehaviour
+{
+    private GameObject teamCredits;
+    private GameObject assetCredits;
+
+    private void Start()
+    {
+        teamCredits = transform.parent.FindChild("Team").gameObject;
+        assetCredits = transform.parent.FindChild("Assets").gameObject;
+    }
+
+    private void OnClick()
+    {
+        if (teamCredits.activeSelf)
+        {
+            teamCredits.SetActive(false);
+            assetCredits.SetActive(true);
+        }
+        else
+        {
+            teamCredits.SetActive(true);
+            assetCredits.SetActive(false);
+        }
+    }
+}
